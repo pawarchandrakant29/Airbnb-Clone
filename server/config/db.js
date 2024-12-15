9091ad94d7df6fd47efcd992fad467741189019e
Chandrakant Pawar <137754266+pawarchandrakant29@users.noreply.github.com>
2024-12-16 02:27:40 +0530

    airbnb-clone

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectWithDB = () => {
   mongoose.set('strictQuery', false);
   mongoose
-    .connect( "mongodb+srv://pandyabhargav707:kkDrfbI06FXFgCJm@cluster1.okvbl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1", {
+    .connect( "mongodb://localhost:27017", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
